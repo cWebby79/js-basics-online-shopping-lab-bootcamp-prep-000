7145424021f6c30f1e372d51c4796d5ad3213161
Chris Webster <cgw57@hotmail.com>
2018-07-18 18:31:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -57,7 +57,12 @@ function total() {
 
 
 function removeFromCart(item) {
-  
+  for ( var i = 0; i < cart.length; i++) {
+    if (cart[i].itemName === item) {
+      cart = [...cart.slice(0,i), ...cart.slice(i+1)];
+      return cart;
+    }
+  }
 }
 
 function placeOrder(cardNumber) {
