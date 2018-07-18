0c7a6664954b009eb9ece757484ef93e05c425e4
Chris Webster <cgw57@hotmail.com>
2018-07-18 19:14:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (cardNumber) {
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
   } else {
     return `Sorry, we don't have a credit card on file for you.`;
   } 
