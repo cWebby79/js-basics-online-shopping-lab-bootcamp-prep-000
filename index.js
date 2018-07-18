f9090d6de6f38c111b9ca9d354f09cbab7d3face
Chris Webster <cgw57@hotmail.com>
2018-07-18 19:10:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -70,5 +70,8 @@ function placeOrder(cardNumber) {
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
   } else {
     return `Sorry, we don't have a credit card on file for you.`;
+  } 
+  for (var i = 0; i < cart.length; i++) {
+        cart.pop();
   }
 }
