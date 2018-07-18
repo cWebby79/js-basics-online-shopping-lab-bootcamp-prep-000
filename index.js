57ab586e73478b74f57d82ab9030a6815540524d
Chris Webster <cgw57@hotmail.com>
2018-07-18 18:58:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -66,5 +66,9 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  
+  if (cardNumber) {
+    
+  }else {
+    return `Sorry, we don't have a credit card on file for you.`;
+  }
 }
