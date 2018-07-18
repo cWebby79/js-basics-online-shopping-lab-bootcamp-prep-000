7c284e39595af3eee723d767241ab17f2c931359
Chris Webster <cgw57@hotmail.com>
2018-07-18 18:40:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ function total() {
 function removeFromCart(item) {
   for ( var i = 0; i < cart.length; i++) {
     if (cart[i].itemName === item) {
-      delete cart[i];
+      delete cart[i].itemName;
       return cart;
     } 
   }
