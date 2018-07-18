3db8612359ceb218bfdda038b89538d9d362ea06
Chris Webster <cgw57@hotmail.com>
2018-07-18 19:08:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (cardNumber) {
-    console.log `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
       for (var i = 0; i < cart.length; i++) {
         cart.pop();
     }
