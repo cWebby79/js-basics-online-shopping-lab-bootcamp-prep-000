816f5077d9bfdeffe59b773f6a0d90d3f212ce73
Chris Webster <cgw57@hotmail.com>
2018-07-18 13:09:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,23 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor((Math.random() * 100) + 1);
+  var newObj = {itemName: item, itemPrice: price};
+  cart.push(newObj);
+  return (`${item} has been added to your cart.`);
 }
 
 function viewCart() {
-  // write your code here
+    if (cart.length === 0) {
+      return "Your shopping cart is empty.";
+    } else if (cart.length ===1) {
+      return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`;
+    } else if (cart.length === 2) {
+      return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`;
+    } else {
+
+      }
+    }
 }
 
 function total() {
