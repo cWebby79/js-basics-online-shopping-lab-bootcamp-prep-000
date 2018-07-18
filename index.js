106830c5898fa65fe3cc426165e2144c59709dd6
Chris Webster <cgw57@hotmail.com>
2018-07-18 14:05:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function viewCart() {
     var newArray = [];
     for (var i = 0; i < cart.length; i++) {
       newArray.push(`In your cart, you have ${cart[i].itemName} at $${cart[i].itemPrice}.`);
-      }
+      } return newArray;
     }
   
 }
